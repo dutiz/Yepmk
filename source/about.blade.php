2765d57a7ee9a87cae5881c7710ddc71518bb7ce
Duti <zaimidaut@gmail.com>
2020-01-10 21:11:17 +0100

    footer bg changed rows and columns

---
+++ source/about.blade.php
@@ -0,0 +1,13 @@
+@extends('_layouts.master')
+
+@section('body')
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            I am about page
+        </div>
+    </div>
+</div>
+
+@endsection
