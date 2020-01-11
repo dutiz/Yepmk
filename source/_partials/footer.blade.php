f361b88a678a40e849c9fba79806a61349c11e3d
Duti <zaimidaut@gmail.com>
2020-01-11 17:35:07 +0100

    Updated background image and footer

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,32 +1,41 @@
-<div class="container">
-    <div class="flex flex-col items-center">
-
-        <h2 class="text-center text-gray-400 font-bold text-3xl px-48">Are you determined and passionate about youth projects and volunteering?</h2>
-        <a href="/contact" class=" inline-block mt-8 rounded bg-yellow-500 text-base px-6 py-3 text-white ">Join Us</a>
+<div style="background: url('/assets/images/footer-bg.png') no-repeat center center / cover;">
+    <div class="container pt-56 pb-10">
+        <div class="flex flex-col items-center">
+            <h2 class="text-center text-white font-bold text-3xl px-48">Are you determined and passionate about youth
+                projects and volunteering?</h2>
+            <a href="/contact" class=" inline-block mt-8 rounded bg-yellow-500 text-base px-6 py-3 text-white ">Join
+                Us</a>
+        </div>
     </div>
 </div>
 
-<div class="container">
-    <div class="row mb-4 mt-10 text-base text-gray-400 pt-20">
-        <div class="col-4">
-            <p>Major Cede Filiposki 158, <br>Gostivar 1230,<br>Republic of N. Macedonia</p>
+<div class="bg-purple-500">
+    <div class="container">
+        <div class="row mb-4 text-base text-white pt-20">
+            <div class="col-4">
+                <p>Major Cede Filiposki 158, <br>Gostivar 1230,<br>Republic of N. Macedonia</p>
+            </div>
+            <div class="col-4">
+                <p class="text-center">Monday - Saturday:<br> 10:00 - 18:00</p>
+            </div>
+            <div class="col-4">
+                <p class="text-right">yepmk2015@gmail.com<br>+389 70 692 549</p>
+            </div>
         </div>
-        <div class="col-4">
-            <p class="text-center">Monday - Saturday:<br> 10:00 - 18:00</p>
-        </div>
-        <div class="col-4">
-            <p class="text-right">yepmk2015@gmail.com<br>+389 70 692 549</p>
-        </div>
-    </div>
-    <div class="flex mb-10 text-base text-gray-400 mt-32">
-        <div class="col-5">
-            <p>© 2019 YEP. All Rights Reserved</p>
-        </div>
-        <div class="col-5 flex justify-end">
-            <a class="mr-8" href="https://www.facebook.com/YEPmk/" target="_blank"><img src="./assets/images/facebook.png" title="Facebook" alt="Facebook"></a>
-            <a class="mr-8" href="http://www.instagram.com/yep.mk" target="_blank"><img src="./assets/images/instagram.png" alt="instagram"></a>
-            <a class="mr-8" href="https://www.youtube.com/channel/UCftepR6yOGtMu5ysdET73SA" target="_blank"><img src="./assets/images/youtube.png" alt="youtube"></a>
-            <a href="https://www.linkedin.com/company/yepmk" target="_blank"><img src="./assets/images/linkedin.png" alt="linkedin"></a>
+        <div class="row justify-between pb-10 text-base text-white mt-32">
+            <div class="col-4">
+                <p>© 2019 YEP. All Rights Reserved</p>
+            </div>
+            <div class="col-5 flex flex-row justify-end">
+                <a class="mr-8" href="https://www.facebook.com/YEPmk/" target="_blank"><img
+                        src="/assets/images/facebook.png" title="Facebook" alt="Facebook"></a>
+                <a class="mr-8" href="http://www.instagram.com/yep.mk" target="_blank"><img
+                        src="/assets/images/instagram.png" alt="instagram"></a>
+                <a class="mr-8" href="https://www.youtube.com/channel/UCftepR6yOGtMu5ysdET73SA" target="_blank"><img
+                        src="/assets/images/youtube.png" alt="youtube"></a>
+                <a href="https://www.linkedin.com/company/yepmk" target="_blank"><img src="/assets/images/linkedin.png"
+                        alt="linkedin"></a>
+            </div>
         </div>
     </div>
 </div>
--- source/index.blade.php
+++ source/index.blade.php
@@ -9,13 +9,14 @@
             <p class="mt-4 text-sm">Learn more about our cause.</p>
             <a href="/about" class=" inline-block mt-8 rounded bg-purple-400 text-base px-6 py-3">Learn More</a>
         </div>
-            Image
+        Image
     </div>
 </div>
 {{-- Icons --}}
 <div class="container">
     <div class="mt-40">
-        <h1 class="text-purple-500 font-bold text-5xl text-center px-40">Empowering today's youth, building tomorrow's leaders ! </h1>
+        <h1 class="text-purple-500 font-bold text-5xl text-center px-40">Empowering today's youth, building tomorrow's
+            leaders ! </h1>
     </div>
     <div class="row mb-4 mt-10 text-center">
         <div class="col-4">
@@ -38,28 +39,29 @@
 {{-- IMPACTS --}}
 <div class="mt-48 container">
     <h1 class="text-purple-500 font-bold text-5xl text-center">Our Impact</h1>
-    <p class=" text-xs  text-center block">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet<br> ad fuga voluptates eligendi, officia consectetur corrupti ipsam?</p>
+    <p class=" text-xs  text-center block">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet<br> ad fuga
+        voluptates eligendi, officia consectetur corrupti ipsam?</p>
 </div>
 <div class="container ">
-    
+
     <div class="flex mb-48 mt-20 mx-48 text-center bg-purple-500 py-8 rounded-lg ">
         <div class="col-3 ">
             <h2 class="text-white font-bold text-6xl ">20</h2>
-                <p class="text-xs text-yellow-500 font-medium">projects completed</p>
-            </div>
-            <div class="col-3 ">
-                <h2 class="text-white font-bold text-6xl">120</h2>
-                <p class="text-xs text-yellow-500 font-medium">volunteers</p>
-            </div>
-            <div class="col-3 ">
-                <h2 class= "text-white font-bold text-6xl">400</h2>
-                <p class="text-xs text-yellow-500 font-medium">events hosted</p>
-            </div>
-            <div class="col-3 ">
-                <h2 class=" text-white font-bold text-6xl">500</h2>
-                <p class="text-xs text-yellow-500 font-medium">hours wasted</p>
-            </div>
+            <p class="text-xs text-yellow-500 font-medium">projects completed</p>
+        </div>
+        <div class="col-3 ">
+            <h2 class="text-white font-bold text-6xl">120</h2>
+            <p class="text-xs text-yellow-500 font-medium">volunteers</p>
+        </div>
+        <div class="col-3 ">
+            <h2 class="text-white font-bold text-6xl">400</h2>
+            <p class="text-xs text-yellow-500 font-medium">events hosted</p>
+        </div>
+        <div class="col-3 ">
+            <h2 class=" text-white font-bold text-6xl">500</h2>
+            <p class="text-xs text-yellow-500 font-medium">hours wasted</p>
         </div>
     </div>
+</div>
 </div>
 @endsection
