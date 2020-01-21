f65c306cf80e38cb18ef4c2cc5e1230891b62a25
lokmanm <lokman.musliu@gmail.com>
2020-01-21 19:57:15 +0100

    Small fixes

--- config.php
+++ config.php
@@ -3,5 +3,23 @@
 return [
     'production' => false,
     'baseUrl' => '',
+    'sponsors' => [
+        [
+            'image' => '/assets/images/komuna.png',
+            'alt' => 'Komuna e Gostivarit',
+        ],
+        [
+            'image' => '/assets/images/usembskopje.png',
+            'alt' => 'US Embassy Skopje',
+        ],
+        [
+            'image' => '/assets/images/peacecorps.png',
+            'alt' => 'Peace Corps',
+        ],
+        [
+            'image' => '/assets/images/usaid.png',
+            'alt' => 'USAID',
+        ],
+    ],
     'collections' => [],
 ];
